d1467954f6896f41aa5a9588d3b294382dd2d596
Jack Christiansen <christj6@tcnj.edu>
2015-03-03 23:08:23 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -25,7 +25,8 @@ import jm.music.tools.*;
 import jm.midi.*;
 import jm.util.*;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
  
 public class Test implements JMC 
@@ -40,10 +41,14 @@ public class Test implements JMC
 
         Score score = new Score();
 
-        Read.midi(score, args[0]);
+        Read.midi(score, args[0]); // check if args[0] is a valid midi file before you try reading it
 
         Mod.quantise(score, 0.25); // institutes barlines everywhere -- seems to work better than the Tools -> Quantize Timing
         // 0.25 corresponds with quarter notes
+
+
+        //String midiFileName = args[0]; // might be changed later on to the name of a modified midi file
+        //runJFugueFile(midiFileName); // 
 
 
 
@@ -53,9 +58,17 @@ public class Test implements JMC
         // and a right-hand finger (p = thumb, i = index, m, a)
         // and also a location on the fretboard (for example, a4 can be played on fret 0 of the a-string or fret 5 of the e-string)
         
-        System.out.println("highest pitch: " + score.getHighestPitch() + ", lowest pitch: " + score.getLowestPitch());
+        //System.out.println("highest pitch: " + score.getHighestPitch() + ", lowest pitch: " + score.getLowestPitch());
         // for standard tuning guitar, the lowest pitch is 40 (e3), and the highest pitch is 84 (c7)
         // idea: transpose the song up/down in such a way to allow in the most notes with minimal truncation (might not work)
+        if (score.getHighestPitch() > 84)
+        {
+            System.out.println("Alert: some note(s) too high for standard-tuned guitar.");
+        }
+        if (score.getLowestPitch() < 40)
+        {
+            System.out.println("Alert: some note(s) too low for standard-tuned guitar.");
+        }
 
         // idea: maximize the occurrences of open strings -- transpose the piece up/down by x semitones until the max # of notes occur on E, A, D, G, b, e strings
 
@@ -65,10 +78,14 @@ public class Test implements JMC
         Phrase phrase = part.getPhrase(0);
         int i, j, k = 0;
 
+        int notesInCurrentChord = 0;
 
-        for (k = 0; k < part.getSize(); k++)
+
+        for (k = 0; k < 40; k++)
         {
-            System.out.println("Chord #" + (k+1));
+            notesInCurrentChord = 0;
+            //System.out.println("Chord #" + (k+1));
+            //int[] whichNotes = new int[4];
 
             for (i = 0; i < score.getSize(); i++)
             {
@@ -92,13 +109,28 @@ public class Test implements JMC
                         System.out.println("pitch: " + pitches[k]);
                     }
                     */
-                    if (k < pitches.length)
+                    if (k < pitches.length && notesInCurrentChord < 4)
                     {
-                        System.out.println("pitch: " + pitches[k]); // outputs pitches for each chord, kinda inefficient
+                        // System.out.println("pitch: " + pitches[k]); // outputs pitches for each chord, kinda inefficient
+
+                        notesInCurrentChord++; // takes another left-hand finger, UNLESS it can be played with an open string
+                        // takes another right-hand finger to pluck the chord, UNLESS the chord is being strummed instead of plucked
                     }
                 }
             }
+
+            if (notesInCurrentChord > 0)
+            {
+                System.out.println("Chord #" + (k+1));
+
+                for (int z = 0; z < notesInCurrentChord; z++)
+                {
+                    // print each note in chord?
+                }
+            }
         }
+
+        
 
         //System.out.println("note array: " + score.getNoteArray());
 
@@ -109,8 +141,28 @@ public class Test implements JMC
 
     }
 
-    public static void func()
+    public static void runJFugueFile(String filename)
     {
-        // do something
+        // alternative idea: use a java process to compile/run a JFugue program, take in the midi file. export it into a txt file storing the music string with numbers
+        // javac -classpath C:\Users\Jack\Documents\spring_2015\jfugue-4.0.3.jar MusicString.java
+        // java -cp C:\Users\Jack\Documents\spring_2015\jfugue-4.0.3.jar;. MusicString 
+
+        // on second thought -- musicString format is pretty gross
+        try // 
+        {
+            String directory = "C:\\Users\\Jack\\Documents\\spring_2015\\jfugue-4.0.3.jar";
+
+            Process pro = Runtime.getRuntime().exec("javac -classpath " + directory + " MusicString.java");
+            //System.out.println(pro.getInputStream());
+            //System.out.println(pro.getErrorStream());
+            pro.waitFor();
+            pro = Runtime.getRuntime().exec("java -cp " + directory + ";. MusicString " + filename);
+            //System.out.println(pro.getInputStream());
+            //System.out.println(pro.getErrorStream());
+            pro.waitFor();
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
     }
 }
