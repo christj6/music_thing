633ee9e8dcd615edb537675841f36fbf92abb2ce
Jack Christiansen <christj6@tcnj.edu>
2015-03-06 10:53:20 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -49,13 +49,13 @@ public class Test implements JMC
         // and a right-hand finger (p = thumb, i = index, m, a)
         // and also a location on the fretboard (for example, a4 can be played on fret 0 of the a-string or fret 5 of the e-string)
         
-        //System.out.println("highest pitch: " + score.getHighestPitch() + ", lowest pitch: " + score.getLowestPitch());
         // for standard tuning guitar, the lowest pitch is 40 (e3), and the highest pitch is 84 (c7)
         // idea: transpose the song up/down in such a way to allow in the most notes with minimal truncation (might not work)
         if (score.getHighestPitch() > 84)
         {
             System.out.println("Alert: some note(s) too high for standard-tuned guitar.");
         }
+
         if (score.getLowestPitch() < 40)
         {
             System.out.println("Alert: some note(s) too low for standard-tuned guitar.");
@@ -63,10 +63,7 @@ public class Test implements JMC
 
         // idea: maximize the occurrences of open strings -- transpose the piece up/down by x semitones until the max # of notes occur on E, A, D, G, b, e strings
 
-        
-        //System.out.println("pitch array: ");
-        //Part part = score.getPart(0);
-        //Phrase phrase = part.getPhrase(0);
+
         
         // this section adapted from http://explodingart.com/jmusic/applications/Midi2text.java
         Enumeration enum1 = score.getPartList().elements(); // changed "enum" to "enum1" to avoid the keyword error
@@ -85,6 +82,7 @@ public class Test implements JMC
 
                     if (note.getPitch() != JMC.REST) 
                     {
+                        /*
                         // start time
                         System.out.println("Start time: " + Double.toString(startTime));
                         // pitch
@@ -94,8 +92,44 @@ public class Test implements JMC
                         // velocity
                         System.out.println("Velocity: " + Integer.toString(note.getDynamic()));
                         System.out.println("-------------------------------------------------");
+                        */
+                        
+                        // gather start times
+                    }
 
-                        // add the note to the bucket thing you were talking about
+                    startTime += note.getDuration();
+                }
+            }
+        }
+
+        // filter out start time array so it just has unique entries
+        // sort start time array
+
+        // create array of linked lists of note objects
+        //
+
+        // 2nd time around
+        enum1 = score.getPartList().elements();
+        while(enum1.hasMoreElements())
+        {
+            Part part = (Part) enum1.nextElement();
+            Enumeration enum2 = part.getPhraseList().elements();
+            while(enum2.hasMoreElements())
+            {
+                Phrase phrase = (Phrase) enum2.nextElement();
+                double startTime = phrase.getStartTime(); 
+                Enumeration enum3 = phrase.getNoteList().elements();
+                while(enum3.hasMoreElements())
+                {
+                    Note note = (Note) enum3.nextElement();
+
+                    if (note.getPitch() != JMC.REST) 
+                    {
+                        // retrieve start time of note
+                        // use it to perform binary search on start time array
+                        // retrieve correct index
+                        // add note to the linked list stored in the correct index
+
                     }
 
                     startTime += note.getDuration();
@@ -123,9 +157,20 @@ public class Test implements JMC
         // for each note in the chord:
         // extract the note's pitch value
         // find pitch on fretboard
-        // for example, pitch 50 (D4) yields a candidate array [-1, -1, -1, 0, 5, 10]
+        // for example, pitch 50 (D4) yields a candidate array [-1, -1, -1, 0, 5, 10] --> [50 - 64, 50 - 59, 50 - 55, 50 - 50, 50 - 45, 50 - 40], disregard negative entries
         // since D4 can be played in 3 ways: play D-string open, play A-string 5th fret, or play E-string 10th fret.
         // D4 cannot be played on any other string, so those slots in the array have -1.
+
+        // for a 3-note chord (for example, C4, E4, G4), the candidate arrays would look like:
+        // G: [x, x, 0, 5, 10, 15]
+        // E: [x, x, x, 2, 7,  12]
+        // C: [x, x, x, x, 3,  8]
+        // we can iterate through the top array until we find an entry that is not -1. If not, we go to next string. 
+        // When we find a valid fret #, we can assign the first finger (index) to that fret/string. Unless it's 0 (open string).
+        // Then we must go to next string to find next note (can't play 2 notes on the same string). 
+        // When we find the next valid fret #, the # must be greater than or equal to the fret # that the index finger is assigned to.
+        // We will assign the next finger (middle) to that and continue on.
+
 
 
         return true; // still need to figure out how the Voicing object will be set up/structured.
@@ -141,9 +186,9 @@ public class Test implements JMC
 
     // given two pitch values (for example, 60 and 72), this function determines if the two pitches are the same note but some # of octaves apart.
     // I don't know where it will come in handy just yet.
-    public static boolean areTheseNotesOctavesApart (int pitch1, int pitch2)
+    public static boolean octavesApart (int pitch1, int pitch2)
     {
-        if ((Math.abs(pitch1 - pitch2) % 12) == 0)
+        if ((Math.abs(pitch1 - pitch2) % 12) == 0) // not sure if I need absolute value here
         {
             return true;
         }
@@ -151,5 +196,7 @@ public class Test implements JMC
         {
             return false;
         }
+
+        // alternative approach: take in an array of pitch values as input, take the sum of them, see if the sum mod 12 is 0
     }
 }
