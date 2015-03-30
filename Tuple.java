48e6734038dceea4584ab00ae16a194f48d79a9f
Jack Christiansen <christj6@tcnj.edu>
2015-03-30 19:55:40 -0400

    Update Tuple.java

--- Tuple.java
+++ Tuple.java
@@ -1,9 +1,22 @@
 
 // Tuple class for left hand finger position on guitar
+
 public class Tuple 
 {
 	private int stringNum;
 	private int fretNum;
+
+	// constants
+	public static final int maximumPolyphony = 6; // 6 strings, can be strummed simultaneously
+
+    public static final int lowEString = 40; // string #6, aka lowest possible note
+    public static final int aString = 45;
+    public static final int dString = 50;
+    public static final int gString = 55;
+    public static final int bString = 59;
+    public static final int highEString = 64; // pitch of string #1
+
+    public static final int highestPossibleNote = 84;
 
 	public Tuple(int stringNum, int fretNum) 
 	{
@@ -19,5 +32,27 @@ public class Tuple
 	public int getFretNum()
 	{
 		return fretNum;
+	}
+
+	// takes in string and fret number, returns pitch value
+	public int getPitch (int stringNum, int fretNum)
+	{
+		switch(stringNum)
+		{
+			case 1:
+				return highEString + fretNum;
+			case 2:
+				return bString + fretNum;
+			case 3:
+				return gString + fretNum;
+			case 4:
+				return dString + fretNum;
+			case 5:
+				return aString + fretNum;
+			case 6:
+				return lowEString + fretNum;
+			default:
+				return -1;
+		}
 	}
 }
