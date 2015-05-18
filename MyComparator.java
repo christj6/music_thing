032499a5a3afe2cba0e6debe4209bf134713781a
Jack Christiansen <christj6@tcnj.edu>
2015-05-18 19:22:38 -0400

    Update MyComparator.java

--- MyComparator.java
+++ MyComparator.java
@@ -1,4 +1,10 @@
 
+/*
+    MyComparator.java
+    
+    Allows Note objects to be sorted by their pitch value.
+*/
+
 import jm.JMC;
 import jm.music.data.*;
 import jm.music.tools.*;
@@ -8,20 +14,20 @@ import jm.util.*;
 import java.io.*;
 import java.util.*;
 
+// allows Note objects to be sorted by pitch value
 public class MyComparator implements Comparator<Note>
+{
+    @Override
+    public int compare(Note first, Note second) 
     {
-        // http://stackoverflow.com/questions/18985209/sorting-arraylist-using-mycomparator-class
-        @Override
-        public int compare(Note first, Note second) 
+        if (first.getPitch() > second.getPitch()) 
         {
-            if (first.getPitch() > second.getPitch()) 
-            {
-                return -1;
-            } 
-            else if (first.getPitch() < second.getPitch()) 
-            {
-                return 1;
-            }
-            return 0;
+            return 1;
+        } 
+        else if (first.getPitch() < second.getPitch()) 
+        {
+            return -1;
         }
+        return 0;
     }
+}
