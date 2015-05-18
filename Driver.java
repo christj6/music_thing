dabfcfe3f1d180611dc1342c8c0dc1a3d712b98e
Jack Christiansen <christj6@tcnj.edu>
2015-05-18 19:22:25 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,26 +1,21 @@
 
-// Jack Christiansen
-
-// using Windows cmd:
-// compile with
-// javac -cp C:\Users\Jack\jmusic\jmusic.jar *.java
-
-// run with
-// java -cp C:\Users\Jack\jmusic\jmusic.jar;. Driver file.mid output.mid
-// whatever the 2nd file is will be overwritten
-
-// to add to the git:
-// delete the folder from local
-// git clone https://github.com/christj6/music_thing
-// cd music_thing
-// git add .
-// git commit -m "some message"
-// git push origin master
+/*
+    Driver.java
+    
+    Allows the user to interact with the program by inputting a midi file of their choice.
+*/
 
 public class Driver
 {
 	public static void main(String[] args)
 	{
-		Test mus = new Test(args[0], args[1]);
+		if (args.length == 1)
+		{
+			Test mus = new Test(args[0]);
+		}
+		else if (args.length == 2)
+		{
+			Test mus = new Test(args[0], args[1]);
+		}
 	}
 }
